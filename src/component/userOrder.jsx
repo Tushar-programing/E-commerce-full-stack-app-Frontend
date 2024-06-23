51c4c52740d88a28a4995398cddf99c027242fb7
Tushar saini <ttushar476@gmail.com>
2024-06-23 23:32:20 +0530

    ui cange

--- src/component/userOrder.jsx
+++ src/component/userOrder.jsx
@@ -34,7 +34,7 @@ function order({adress1, adress2, city, company, country, createdAt, name, owner
             {/* <p className='mt-1 ml-2 '>Status : <span className={` text-xl ${status === "cancel"? 'text-red-600': 'text-green-600'}`}>{status}</span></p> */}
           </div></Link>
           <Link to={`/userpro/${_id}`}><div className=' w-28 '>
-            <p className='mt-6 text-gray-800 text-center '>{quantity} pcs</p>
+            <p className='mt-6 text-green-500 text-center '>{quantity} pcs</p>
             {/* <p className='mt-2 ml-4 '>price : <span className='text-gray-800'>₹ {product_details?.price}</span></p>
             <p className='mt-2 ml-4'>Total : <span className='text-red-600  text-lg'>₹ {product_details.price * quantity}</span></p> */}
           </div></Link>
